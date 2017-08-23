eaba31fa2095016e269a9b3a6a50e8b746eac38d
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-22 21:52:06 -0700

    omg i think i finished...

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style/main.scss';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import superagent from 'superagent';
 
 const API_URL = 'http://www.reddit.com/r';
@@ -9,133 +9,134 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: '',
-      limit: '',
       topics: [],
-      failed: false
-    }
+    };
+    this.result = this.result.bind(this);
   }
 
-
-  render() {
-    return (
-      <div>
-        <SearchResultsList />
-      </div>
-    );
-  }
-}
-
-class SearchForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board: '',
-      limit: '',
-      topics: [],
-      failed: false
-    }
-
-    this.handleBoardChange = this.handleBoardChange.bind(this);
-    this.handleLimitChange = this.handleLimitChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  componentDidUpdate() {
+    console.log('__STATE__', this.state);
   }
 
-  handleBoardChange(e) {
-    this.setState({board: e.target.value})
-  }
-
-  handleLimitChange(e) {
-    this.setState({limit: e.target.value})
-  }
-
-  handleSubmit(e) {
-    console.log('hi');
-    e.preventDefault();
-    this.props.handleSearch(this.state.board, this.state.limit)
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit} >
-          <input
-            type='text'
-            name='boardName'
-            placeholder='Reddit Board Name'
-            value={this.state.board}
-            onChange={this.handleBoardChange}
-            />
-          <input
-            type='number'
-            name='boardLimit'
-            placeholder='number'
-            value={this.state.limit}
-            onChange={this.handleLimitChange}
-            min='0'
-            max="100"
-            />
-          <input
-            type="submit"
-            value="submit"
-            />
-        </form>
-      </div>
-    );
-  }
-}
-
-class SearchResultsList extends React.Component{
-  constructor(props) {
-    super(props);
-
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-
-  handleSearch(searchFormBoard, searchFormLimit) {
-    console.log(searchFormLimit);
-    superagent.get(`${API_URL}/${searchFormBoard}.json?limit=${searchFormLimit}`)
-    .then( res => {
-      let topics = res.body.data.children.reduce((processedPosts, post) => {
-        let newPost = {
-          title: post.data.title,
-          url: post.data.url,
-          ups: post.data.ups
-        }
-
-        processedPosts.push(newPost);
-
-        return processedPosts
-      }, []);
-      try {
-        this.setState({
-          topics,
-          failed: false
-        });
-        console.log(topics);
-      } catch (err) {
-        console.error(err);
-        this.setState({ failed: true })
-        console.log('1', this.state);
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      this.setState({ failed: true })
-      console.log('2', this.state);
+  result(redditResults) {
+    this.setState({
+      topics: redditResults,
     });
   }
-
   render() {
     return (
       <div>
         <h1>Reddit Search Engine</h1>
-        <SearchForm handleSearch={this.handleSearch} />
+        <SearchForm result = {this.result} />
+        <SearchResultList topics = {this.state.topics} />
+      </div>
+    );
+  }
+}
+class SearchForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      board: '',
+      limit: '',
+      failed: null,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBoardNameChange = this.handleBoardNameChange.bind(this);
+    this.handleLimitChange = this.handleLimitChange.bind(this);
+  }
+  handleBoardNameChange(event) {
+    this.setState({board: event.target.value});
+  }
+
+  handleLimitChange(event) {
+    this.setState({limit: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    superagent.get(`${API_URL}/${this.state.board}.json?limit=${this.state.limit}`)
+      .then((res) => {
+        let topics = res.body.data.children.reduce((processedPosts, post) => {
+       let newPost = {
+         title: post.data.title,
+         url: post.data.url,
+         ups: post.data.ups
+       }
+
+       processedPosts.push(newPost);
+
+       return processedPosts
+     }, []);
+     try {
+       this.props.result(topics);
+     } catch (err) {
+       console.error(err);
+       this.setState({ failed: true })
+       console.log('1', this.state);
+     }
+   })
+      .catch((err) => {
+        this.setState({
+          failed: true,
+        });
+      });
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}  >
+        <input
+          className={ this.state.failed ? 'error' : ''}
+          type='text'
+          name='board'
+          placeholder='Board Name'
+          value={this.state.board}
+          onChange={this.handleBoardNameChange}
+        />
+        <input
+          className={ this.state.failed ? 'error' : ''}
+          type='number'
+          min='0'
+          max='100'
+          name='limit'
+          placeholder='Limit [0-100]'
+          value={this.state.limit}
+          onChange={this.handleLimitChange}
+          min='0'
+          max='100'
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+class SearchResultList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+          <ul>
+            {this.props.topics.map((item, i) => {
+              return (
+                <li key = {i}>
+                  <a href = {item.url}>
+                    <h2>{item.title}</h2>
+                  </a>
+                  <span>
+                    <p>{item.ups}</p>
+                  </span>
+                </li>
+              );
+            }
+            )}
+          </ul>
       </div>
     );
   }
 }
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
