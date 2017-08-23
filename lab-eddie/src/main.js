278646f354b9540fed5328d8ccdd40cc6066421a
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-08-22 17:38:30 -0700

    Wrote bare minimum to get webpack to build

---
+++ lab-eddie/src/main.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import ReactDom from 'react-dom';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+
+ReactDom.render(<App />, document.getElementById('root'));
