d2a9decebe0762020e12c47f3f0f1fc08361dc2c
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-22 18:13:39 -0700

    checkpoint 6:13pm refactoring main.js

--- src/main.js
+++ src/main.js
@@ -2,89 +2,98 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import superagent from 'superagent';
 
-const API_URL = 'http://reddit.com/r';
-
-class RedditForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      redditSearch: '',
-      redditLimit: ''
-    }
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleRedditSearchChange = this.handleRedditSearchChange.bind(this);
-    this.handleRedditLimitChange = this.handleRedditLimitChange.bind(this);
-
-  }
-
-  handleRedditSearchChange(e) {
-    this.setState({redditSearch: e.target.value})
-  }
-
-  handleRedditLimitChange(e) {
-    this.setState({redditLimit: e.target.value})
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state.redditSearch, this.state.redditLimit);
-    // this.props.redditSelect(this.state.redditSearch, this.state.redditLimit);
-  }
-
-  render() {
-    return (
-      <section>
-
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type='text'
-            name='redditSearch'
-            placeholder='Search for a Reddit Board'
-            value={this.state.redditSearch}
-            onChange={this.handleRedditSearchChange} />
-          <input
-            type='number'
-            name='redditLimit'
-            placeholder='Enter a number betweek 1 and 100'
-            value={this.state.redditLimit}
-            onChange={this.handleRedditLimitChange} />
-        </form>
-      </section>
-    )
-  }
-}
+const API_URL = 'http://www.reddit.com/r';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      redditLookup: {},
-      redditSearchQuery: null,
-      redditSearchQueryError: null,
-      redditSearchLimit: null,
-      redditSearchLimitError: null
+      board = '',
+      limit = '',
+      topics = [],
+      failed = false;
     }
 
-    // TODO
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBoardChange = this.handleBoardChange.bind(this);
+    this.handleLimitChange = this.handleLimitChange.bind(this);
+
   }
 
-  componentDidUpdate() {
-    console.log('__STATE__', this.state);
+  handleBoardChange(e) {
+    this.setState({board: e.target.value})
   }
 
-  redditSelect(name,limit) {
-    if(localStorage.redditLookup) {
-      try {
-        let pokemonLookup = JSON.parse(localStorage.redditLookup);
-        this.setState({redditLookup})
-      } catch (err){
-        console.error(err);
-      }
-    } else {
-      superagent.get
-    }
+  handleLimitChange(e) {
+    this.setState({limit: e.target.value})
+  }
+
+  handleSubmit(e) {
+    console.log('hi');
+    e.preventDefault();
   }
 }
 
-ReactDOM.render(<RedditForm />, document.getElementById('root'))
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: '',
+      limit: '',
+      posts: []
+    }
+
+  handleSearch(searchFormBoard, searchFormLimit) {
+    superagent.get(`${API_URL}/${searchFormBoard}.json?limit=${this.state.limit}`)
+    .then( res => {
+      let posts = res.body.data.children.reduce((processedPosts, post) => {
+        let newPost = {
+          title: post.data.title,
+          url: post.data.url,
+          ups: post.data.ups
+        }
+
+        processedPosts.push(newPost);
+
+        return processedPosts
+      }, []);
+      try {
+        console.log('hello');
+        this.setState({ posts })
+        console.log(posts);
+      } catch (err) {
+        console.error(err);
+      }
+    })
+    .catch(console.error);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <input
+          type='text'
+          name='boardName'
+          placeholder='Reddit Board Name'
+          value={this.state.board}
+          onChange={this.handleBoardChange}
+        />
+        <input
+          type='number'
+          name='boardLimit'
+          placeholder='number'
+          value={this.state.limit}
+          onChange={this.handleLimitChange}
+          min="1"
+          max="100"
+        />
+      <input type="submit" value="submit"/>
+      </form>
+    );
+  }
+}
+
+
+
+
+ReactDOM.render(<SearchForm />, document.getElementById('root'));
